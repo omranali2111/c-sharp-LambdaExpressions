160913cf5883f447c5fa330450864017d117b117
omranali2111 <aljabri1234@hotmail.com>
2023-07-24 09:51:45 +0400

    get square of numbers using lambda

--- c-sharp-LambdaExpressions/Program.cs
+++ c-sharp-LambdaExpressions/Program.cs
@@ -2,6 +2,17 @@
 {
     private static void Main(string[] args)
     {
-      
+        List<int> numbers = new List<int>() {1, 2, 3,
+                             4, 5, 6, 7, 8, 9, 10};
+
+        var square = numbers.Select(x => x * x);
+
+        // foreach loop to display squares
+        Console.Write("Squares : ");
+        foreach (var value in square)
+        {
+            Console.Write( value+" ");
+        }
+        Console.WriteLine();
     }
 }
